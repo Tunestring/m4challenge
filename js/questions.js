a4ce8e637a6255b500afdc844223473d16759d7f
Nate Long <nate.k.long@gmail.com>
2023-02-01 11:25:00 -0500

    initial commit

---
+++ js/questions.js
@@ -0,0 +1,57 @@
+var questions = [
+  {
+    title: "Inside which HTML element do we put the JavaScript?",
+    choices: ["<js>", "<scripting>", "<script>", "<javascript>"],
+    answer: "<script>"
+  },
+  {
+    title: "Where is the correct place to insert a JavaScript?",
+    choices: ["The <body> section","The <head> section","Both the <head> and <body> section","None of the above"],
+    answer: "The <body> section"
+  },
+  {
+    title: "The external JavaScript file must contain the <script> tag.",
+    choices: ["True", "False"],
+    answer: "True"
+  },
+  {
+    title: "JavaScript is a ___ -side programming language.",
+    choices: ["Client","Server","Both","None"],
+    answer: "Both"
+  },
+  {
+    title:"Which built-in method returns the length of the string?",
+    choices: ["length()","size()","index()","None of the above"],
+    answer: "length()"
+  },
+  {
+    title: "Which built-in method reverses the order of the elements of an array?",
+    choices: ["changeOrder(order)","reverse()","soft(order)","None of the above"],
+    answer: "reverse()"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  },
+  {
+    title: "Arrays in JavaScript can be used to store ____.",
+    choices: ["numbers and strings","other arrays","booleans","all of the above"],
+    answer: "all of the above"
+  },
+  {
+    title:"String values must be enclosed within ____ when being assigned to variables.",
+    choices: ["commas", "curly brackets", "quotes", "parentheses"],
+    answer: "quotes"
+  },
+  {
+    title:"A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+    answer: "console.log"
+  },
+  {
+    title: "What is the type of Pop up boxes available in JavaScript?:",
+    choices: ["Alert", "Confirm", "Prompt", "All the above"],
+    answer: "All the above"
+  }
+];
